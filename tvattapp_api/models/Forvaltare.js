b1abfbfdc0e6d23f24d68440a7d3917cf3c79b31
Daniel Mamgren de Oliveira <danielmamgrendeoliveira@Daniels-MacBook-Pro.local>
2020-09-13 20:21:27 +0200

    Added Forvaltare model

--- tvattapp_api/models/Forvaltare.js
+++ tvattapp_api/models/Forvaltare.js
@@ -11,20 +11,50 @@ const ForvaltareSchema = new mongoose.Schema({
 			'Namnet kan inte överstiga 50 tecken',
 		],
 	},
-	condominios: {},
+	// condominios: {},
 	logo: {
-		type: Object,
+		type: String,
 		required: [true, 'Logotyp är obligatorisk'],
 		unique: true,
+		default: 'no-logo.jpg',
 	},
 	url: {
 		type: String,
 		match: [
-			/https?:|\/\(www\.)?[a-zA-Z0-9@:%_\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-+()@:%_\+.~#?&//=]*)/,
+			/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)/,
 			'Använd en befintlig websida / URL med HTTP eller HTTPS',
 		],
 		unique: true,
 		trim: true,
 	},
 	slug: String,
+
+	// For Condominio later
+	// location: {
+	// 	//GeoJSON Point
+	// 	type: {
+	// 		type: String,
+	// 		enum: ['Point'],
+	// 		required: true,
+	// 	},
+	// 	coordinates: {
+	// 		type: [Number],
+	// 		required: true,
+	// 		index: '2dsphere',
+	// 	},
+	// 	formattedAddress: String,
+	// 	street: String,
+	// 	city: String,
+	// 	state: String,
+	// 	country: String,
+	// },
+	createdAt: {
+		type: Date,
+		default: Date.now,
+	},
 });
+
+module.exports = mongoose.model(
+	'Forvaltare',
+	ForvaltareSchema
+);
