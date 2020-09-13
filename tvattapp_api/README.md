333d2e56a61dfc1709fb6b755173f75a025c7f64
Daniel Mamgren de Oliveira <danielmamgrendeoliveira@Daniels-MacBook-Pro.local>
2020-09-13 15:47:45 +0200

    Added database connection to MongoDB

--- tvattapp_api/README.md
+++ tvattapp_api/README.md
@@ -1,26 +1,35 @@
 # Tvätt App (www.mintvatttid.nu)
+
 ### Boka tvätttid på nätet.
+
 ### Kommunicera tillgänglighet.
 
 ## Stack
-#### Node 
+
+#### Node
+
 #### Express
+
 #### MongoDB
+
 #### Mongoose
+
 #### React
 
-
 ## Environment
+
 #### Node
+
 #### NPM
+
     - Nodemon
-    - 
+    - Morgan
+
 #### VSCode
+
     - Auto Rename Tag
     - Bracket Pair Colorizer
     - DotEnv
     - Express Snippets
     - JavaScript Snippets
     - Prettier
-
-
--- tvattapp_api/server.js
+++ tvattapp_api/server.js
@@ -1,15 +1,19 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
+const connectDB = require('./config/db');
+
+//Load ENV
+dotenv.config({ path: './config/config.env' });
+
+// Connect to MongoDB
+connectDB();
 
 // Route files
 // const anvandare = require('./routes/anvandare');
 // const fastighet = require('./routes/fastighet');
 const forvaltare = require('./routes/forvaltare');
 // const tvattstuga = require('./routes/tvattstuga');
-
-//Load ENV
-dotenv.config({ path: './config/config.env' });
 
 //Initialte express
 const app = express();
@@ -26,9 +30,19 @@ app.use('/api/v1/forvaltare', forvaltare);
 const PORT = process.env.PORT || 5000;
 
 // Initialize server
-app.listen(
+const server = app.listen(
 	PORT,
 	console.log(
 		`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
 	)
+);
+
+//Rejection handling
+process.on(
+	'unhandledRejection',
+	(err, promise) => {
+		console.log(`Error: ${err}`);
+		//Close server
+		server.close(() => process.exit(1));
+	}
 );
