b01dd661dc0511f38c18f25ebe4b440e951d3c67
Daniel Mamgren de Oliveira <danielmamgrendeoliveira@Daniels-MacBook-Pro.local>
2020-09-13 12:29:54 +0200

    Added HTTP loggin middleware - morgan

--- tvattapp_api/server.js
+++ tvattapp_api/server.js
@@ -1,23 +1,34 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
 
-// Route files 
+// Route files
 // const anvandare = require('./routes/anvandare');
 // const fastighet = require('./routes/fastighet');
 const forvaltare = require('./routes/forvaltare');
 // const tvattstuga = require('./routes/tvattstuga');
 
 //Load ENV
-dotenv.config({path: './config/config.env'});
+dotenv.config({ path: './config/config.env' });
 
 //Initialte express
 const app = express();
 
+//Middleware logging - using Morgan
+if (process.env.NODE_ENV === 'development') {
+	app.use(morgan('dev'));
+}
+
 //Mounting routes
-app.use('/api/v1/forvaltare', forvaltare)
+app.use('/api/v1/forvaltare', forvaltare);
 
 //Assign port - conditional on dev or prod
 const PORT = process.env.PORT || 5000;
 
 // Initialize server
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+app.listen(
+	PORT,
+	console.log(
+		`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`
+	)
+);
