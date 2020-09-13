97b7fd5a2c26d70c01d9d1e4150e8030930a45d8
Daniel Mamgren de Oliveira <danielmamgrendeoliveira@Daniels-MacBook-Pro.local>
2020-09-13 11:21:01 +0200

    Set up basic initial route structure

--- tvattapp_api/server.js
+++ tvattapp_api/server.js
@@ -7,6 +7,27 @@ dotenv.config({path: './config/config.env'});
 
 const app = express();
 
+//ROUTES
+app.get('/api/v1/forvaltare', (req, res) => {
+    res.status(200).json({success: true, msg: 'Visa alla fastighetsförvaltare'});
+});
+
+app.get('/api/v1/forvaltare/:id', (req, res) => {
+    res.status(200).json({success: true, msg: `Visa fastighetsförvaltare ${req.params.id}`});
+});
+
+app.post('/api/v1/forvaltare', (req, res) => {
+    res.status(200).json({success: true, msg: 'Läg till en ny fastighetsförvaltare'});
+});
+
+app.put('/api/v1/forvaltare/:id', (req, res) => {
+    res.status(200).json({success: true, msg: `Uppdatera fastighetsförvaltare ${req.params.id}`});
+});
+
+app.delete('/api/v1/forvaltare/:id', (req, res) => {
+    res.status(200).json({success: true, msg: `Ta bort fastighetsförvaltare ${req.params.id}`});
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
