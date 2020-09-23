0267b866b71961f86773eee6939cae8dcb9e026e
Daniel Mamgren de Oliveira <danielmamgrendeoliveira@Daniels-MacBook-Pro.local>
2020-09-23 19:41:07 +0200

    Added the express-mongo-sanitize library

--- tvattapp_api/routes/auth.js
+++ tvattapp_api/routes/auth.js
@@ -16,8 +16,8 @@ const {
 
 router.post('/registrera', registrera);
 router.post('/login', login);
-router.get('/logout', logout);
 router.get('/me', protect, getMe);
+router.get('/logout', logout);
 router.put('/updatedetails', protect, updateDetails);
 router.put('/updatepassword', protect, updatePassword);
 router.post('/forgotpassword', forgotPassword);
